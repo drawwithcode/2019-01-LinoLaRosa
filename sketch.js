9ed4f6388665b84ff62daeb9ebe0fb4e1f9c1249
Michele La Rosa <miki.larosa@hotmail.it>
2019-09-24 15:31:22 +0200

    Aggiunta sfondi colorati

--- sketch.js
+++ sketch.js
@@ -17,6 +17,10 @@ function draw() {
   stroke(0);
   line(250, 0, 250*cos(frameCount*2), 250*sin(frameCount*2));
 
+  if (frameCount == 180){
+    background("purple");
+  }
+
   if (frameCount > 180){
     strokeWeight(3);
     strokeCap(SQUARE);
@@ -25,12 +29,20 @@ function draw() {
     line(50, 0, 50*cos(frameCount*7.2), 50*sin(frameCount*7.2));
     }
 
+  if (frameCount == 360){
+      background("teal");
+    }
+
   if (frameCount > 360){
       strokeWeight(6);
       strokeCap(ROUND);
       stroke("purple");
       translate(-1000, 0);
       line(50, 0, 50*cos(frameCount*7.2), 50*sin(frameCount*7.2));
+      }
+
+  if (frameCount == 540){
+      background("orange");
       }
 
   if (frameCount > 540){
