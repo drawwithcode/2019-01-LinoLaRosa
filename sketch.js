9081596fb825c5065a5acc749c413c3123c81971
Michele La Rosa <miki.larosa@hotmail.it>
2019-09-24 14:58:37 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,9 +3,39 @@ function preload(){
 }
 
 function setup() {
-  // put setup code here
+
+  createCanvas (windowWidth, windowHeight);
+  background(255);
+  angleMode(DEGREES);
+  frameRate(60);
+
 }
 
 function draw() {
-  // put drawing code here
+
+  translate(width/2, height/2);
+  stroke(0);
+  line(250, 0, 250*cos(frameCount*2), 250*sin(frameCount*2));
+
+  if (frameCount > 180){
+    hue++;
+    strokeWeight(3);
+    strokeCap(SQUARE);
+    stroke("teal");
+    translate(500, 0)
+    line(50, 0, 50*cos(frameCount*7.2), 50*sin(frameCount*7.2));
+    }
+
+  if (frameCount > 360){
+      strokeWeight(6);
+      strokeCap(ROUND);
+      stroke("purple");
+      translate(-1000, 0)
+      line(50, 0, 50*cos(frameCount*7.2), 50*sin(frameCount*7.2));
+      }
+
+  if (frameCount > 540){
+    strokeWeight(10);
+  }
+
 }
